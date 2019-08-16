8d8882b7ed10cdf0623d2d2790cc570fb3c0deb1
Daphne Magsby <daphnem@crossbrowsertesting.com>
2019-08-16 10:44:05 -0500

    first commit

---
+++ BasicExample.java
@@ -0,0 +1,47 @@
+ import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.remote.*;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+class BasicExample {
+
+	  private WebDriver driver;
+
+	    @BeforeEach
+	    void setUp() throws Exception {
+	        String username = "YOUR_USERNAME";
+	        String authkey = "YOUR_AUTHKEY";
+	        DesiredCapabilities capabilities = new DesiredCapabilities();
+	        capabilities.setCapability("version", "76");
+	        capabilities.setCapability("platform", "WINDOWS");
+	        capabilities.setCapability("name", "Junit Example");
+	        capabilities.setCapability("browserName", "Chrome");
+	        capabilities.setCapability("record_video", true);
+
+	        String hubAddress = String.format("http://%s:%s@hub.crossbrowsertesting.com:80/wd/hub", username, authkey);
+	        URL url = new URL(hubAddress);
+
+	        driver = new RemoteWebDriver(url, capabilities); 
+	        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	    
+	    }
+
+	    @AfterEach
+	    void tearDown() throws Exception {
+	        driver.quit();
+	    
+	    }
+
+	    @Test
+	    void test() {
+	        
+	        driver.get("http://www.google.com");
+	        assertEquals("Google", driver.getTitle());
+	    
+	    }
+
+}
